8ef26bca269f24c4ee266941d4ecc147d2a87cfb
geremygustavo <abelmallcu123@gmail.com>
2019-03-04 15:47:23 -0400

    mod unit test

--- store-webapp-sample/src/test/java/org/seedstack/samples/store/interfaces/rest/product/ProductsResourceTest.java
+++ store-webapp-sample/src/test/java/org/seedstack/samples/store/interfaces/rest/product/ProductsResourceTest.java
@@ -3,19 +3,22 @@ package org.seedstack.samples.store.interfaces.rest.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class ProductsResourceTest {
     ProductsResource p;
-
+    ProductRepresentation prod = new ProductRepresentation();
 
     @Before
     public void before() {
         p = new ProductsResource();
     }
 
-    @Test
+    @Test(expected = NullPointerException.class)
     public void test1() {
-        assertEquals(null, p.getProduct(1));
+        p.getProduct(12345678910L);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void test2() {
+        p.getProduct(232L);
     }
 }
